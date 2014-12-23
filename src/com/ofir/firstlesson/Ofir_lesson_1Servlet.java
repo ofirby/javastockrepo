be003d440b59848d6a9faaa5614da8d7b0a34f16
ofirby <ofirbenyaacov@gmail.com>
2014-12-23 18:19:17 +0200

    Made Exercise 02 Define:Num1, Num2, Num3

--- src/com/ofir/firstlesson/Ofir_lesson_1Servlet.java
+++ src/com/ofir/firstlesson/Ofir_lesson_1Servlet.java
@@ -5,7 +5,15 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class Ofir_lesson_1Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		int num1 = 4;
+		int num2 = 3;
+		int num3 = 7;
+		int num4 = num1 + num2;
+		int num5 = num4 * num3;
+		 
+		
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.getWriter().println("Result of (" + num1 + "+" + num2 + ") * " + num3 + " = " + num5);
 	}
 }
